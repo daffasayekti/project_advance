80b89d356113e2d22f462f03cb8c200fc27484c1
daffasayekti <daffasayekti0206@gmail.com>
2022-10-11 18:43:14 +0700

    edit jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class User extends Model implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $connection = "mongodb";
+    protected $collection = "users";
+    protected $fillable   = ['name', 'email', 'password'];
 
     public function getJWTCustomClaims()
     {
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,16 @@
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::prefix('todoapps')->group(function () {
     Route::get('/show_todo', [TodoController::class, 'showTodo']);
     Route::post('/create_todo', [TodoController::class, 'createTodo']);
     Route::put('/update_todo/{id}', [TodoController::class, 'updateTodo']);
     Route::delete('/delete_todo/{id}', [TodoController::class, 'deleteTodo']);
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
